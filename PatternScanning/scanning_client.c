1598185a9cd80c1d4fd01718a99b0634a0327f4a
Harsh Pooniwala <54071824+sixtyferret@users.noreply.github.com>
2020-11-30 21:13:23 +0530

    testing git

--- PatternScanning/scanning_client.c
+++ PatternScanning/scanning_client.c
@@ -11,18 +11,16 @@ int main(int argc, char *argv[])
 	char kw[50];char st1[50];char st2[50];
 	/*st1 and st2 are two key words extracted from kw after removing the condition like OR/AND
 	  If no condition then kw stores the key words*/
+	printf("peepeepoopoo");
 	
 	strcpy(kw, argv[1]);
 	//kw stores a copy of the key words to be searched in the files
-	
+
 	lower_string(kw);
 	int flag=check(kw,st1,st2);
 	//flag tells if the kw has OR/AND/None of the above
-	 
+
 	search(argc,flag,kw,st1,st2,&argv[0]);
-	//&argv[0] passes address of first element of array 
+	//&argv[0] passes address of first element of array
 	return 0;
 }
-
-
-
